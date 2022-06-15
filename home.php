b577a86a6f5bc1dea34b3a730c5be47ae51842aa
Mawar Sry Dewi <103552270+MawarDewi@users.noreply.github.com>
2022-06-15 14:10:15 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Toko Sembako</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="css/style.css">
+
+	</head>
+	<body>
+	<div id="colorlib-page">
+		<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+		<aside id="colorlib-aside" role="complementary" class="js-fullheight">
+			<nav id="colorlib-main-menu" role="navigation">
+				<ul>
+					<li class="colorlib-active"><a href="">Home</a></li>
+					<li><a href="../menu.php">Menu</a></li>
+					<li><a href="">Contact</a></li>
+					<li><a href="../index.php">Log out</a></li>
+				</ul>
+			</nav>
+		</aside> <!-- END COLORLIB-ASIDE -->
+
+		<div id="colorlib-main">
+			<section class="ftco-section pt-4 mb-5 ftco-intro">
+				<div class="container-fluid px-3 px-md-0">
+					<div class="row">
+						<div class="col-md-12 mb-4">
+							<h1 class="h2">Perkenalan Toko Sembako </h1>
+							<p>Produk yang ditawarkan merupakan bahan kebutuhan pokok masyarakat dan harga barang yang dijual dengan harga yang terjangkau</p>
+						</div>
+						<div class="col-md-6">
+							<h2 class="h4">Penjualan Produk</h2>
+							<p>Toko Sembako ini menjual berbagai macam barang dan bahan makanan yang mungkin bisa kita dapatkan juga di toko-toko, 
+							seperti: Sembako terdiri dari Gula Pasir, Cabai, Bawang Merah, Bawang Putih, Lengkuas, Jahe, Sayur-sayuran, Buah-buahan, Beras,Tepung Terigu, Telur Ayam, Telur Bebek
+							Makanan Ringan : Jajanan, Minuman Kaleng, Makanan Kaleng, Roti,dll
+							Alat-alat Rumah Tangga :Panci, Wajan, Soled, Baskom, Ember, Centong, Piring, Gelas, Sendok, Garpu, dll.</p>
+						</div>
+						</div>
+		
+					</div>
+				</div>
+			</section>
+		</div><!-- END COLORLIB-MAIN -->
+	</div><!-- END COLORLIB-PAGE -->
+
+	</section>
+
+	<script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+
+	</body>
+</html>
+
